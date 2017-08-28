9084b3b691d4518343afd72281cda9b23f0e1490
Alexey Zhuchkov <zer0divisi1@gmail.com>
2017-08-28 18:53:46 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -ex
 go get -v .
-GOOS=linux GOARCH=amd64 go build -o server .
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags '-s -extldflags "-static"' -o server
 docker build -t hlcup1 .
